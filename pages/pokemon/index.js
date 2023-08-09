9ae8c3f172baf7be565483f99ed51f4ca7786c07
Nils Fischer <nils@designatic.de>
2023-08-09 18:44:27 +0200

    setup fetch and display of pokemon list as like on main index now

--- pages/pokemon/index.js
+++ pages/pokemon/index.js
@@ -1,3 +1,18 @@
 import useSWR from "swr";
 
-export default function PokemonPage() {}
+export default function PokemonPage() {
+  const { data, isLoading, error } = useSWR(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error</div>;
+  const { results } = data;
+  return (
+    <>
+      <h1>Pokemons</h1>
+      {results.map((result) => (
+        <p key={result.name}>{result.name}</p>
+      ))}
+    </>
+  );
+}
