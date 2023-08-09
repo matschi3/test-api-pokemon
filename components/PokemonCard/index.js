8ddbc4a1a9b9f918ac92f44f3cff84845b8fae90
Nils Fischer <nils@designatic.de>
2023-08-09 17:34:52 +0200

    add next's Image component for display pokemons image on card

--- components/PokemonCard/index.js
+++ components/PokemonCard/index.js
@@ -1,8 +1,17 @@
+import Image from "next/image";
+
 export default function PokemonCard({ pokemon }) {
   console.log(pokemon);
+  const { name, sprites } = pokemon;
   return (
     <article>
-      <h2>NAME</h2>
+      <h2>{name}</h2>
+      <Image
+        src={sprites.other["official-artwork"].front_default}
+        alt={name}
+        width={100}
+        height={100}
+      />
     </article>
   );
 }
