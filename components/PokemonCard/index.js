17cd40ae574d52f57b7124c5819bfc3ba84a096c
Nils Fischer <nils@designatic.de>
2023-08-09 19:07:04 +0200

    add link back to pokemon list

--- components/PokemonCard/index.js
+++ components/PokemonCard/index.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function PokemonCard({ pokemon }) {
   const { name, sprites, types } = pokemon;
   return (
     <article>
+      <Link href="/pokemon">back</Link>
       <h2>{name}</h2>
       <Image
         src={sprites.other["official-artwork"].front_default}
