a445fdf19e1613f7e4c3b102137953315e85b0df
Nils Fischer <nils@designatic.de>
2023-08-09 17:12:04 +0200

    renamed fetched data as what it is, pokemon

--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -4,10 +4,12 @@ import { useRouter } from "next/router";
 export default function DetailPokemonPage() {
   const router = useRouter();
   const { id } = router.query;
-  const { data, isLoading, error } = useSWR(
-    `https://pokeapi.co/api/v2/pokemon/${id}`
-  );
+  const {
+    data: pokemon,
+    isLoading,
+    error,
+  } = useSWR(`https://pokeapi.co/api/v2/pokemon/${id}`);
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error</div>;
-  console.log(data);
+  console.log(pokemon);
 }
