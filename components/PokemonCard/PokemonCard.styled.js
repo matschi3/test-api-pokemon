e4ea9042019116551bf5c5715f51aa7758a255e7
Nils Fischer <nils@designatic.de>
2023-08-19 21:42:23 +0200

    add CardContainer + rename old as Card now

--- components/PokemonCard/PokemonCard.styled.js
+++ components/PokemonCard/PokemonCard.styled.js
@@ -1,8 +1,24 @@
 import styled from "styled-components";
 import Image from "next/image";
 
-export const CardContainer = styled.article``;
+export const CardContainer = styled.section``;
 
-export const BackgroundBall = styled.span``;
+export const Card = styled.article`
+  border: solid 1px red;
+  width: 95vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+`;
+
+export const BackgroundBall = styled.span`
+  background-color: ${(props) => props.color || "lightgrey"};
+  border-radius: 50%;
+  width: 80vw;
+  height: 80vw;
+  position: absolute;
+  top: -40vw;
+`;
 
 export const CardImage = styled(Image)``;
