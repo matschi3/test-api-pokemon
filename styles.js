8ba3323498d7c3b585042a2b70c29064f8b95ac0
Nils Fischer <nils@designatic.de>
2023-08-19 18:34:57 +0200

    add global color variables

--- styles.js
+++ styles.js
@@ -7,8 +7,15 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
 
+:root { 
+    --color-white: #eaeaea;
+    --color-black: #121212;
+    --color-main: rgb(255, 74, 17); 
+}
+
   body {
     margin: 0;
     font-family: system-ui;
   }
+
 `;
