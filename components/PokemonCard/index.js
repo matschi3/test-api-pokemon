507cf50c813dd1c1ae3d524e5bb29a01ea74aa0e
Nils Fischer <nils@designatic.de>
2023-08-19 19:08:08 +0200

    add cardImage for styling

--- components/PokemonCard/index.js
+++ components/PokemonCard/index.js
@@ -1,41 +1,49 @@
 import Image from "next/image";
 import Link from "next/link";
-import { CardContainer, BackgroundBall } from "./PokemonCard.styled";
+import { CardContainer, BackgroundBall, CardImage } from "./PokemonCard.styled";
 
 export default function PokemonCard({ pokemon, species }) {
   const { name, sprites, types, stats } = pokemon;
   return (
-    <article>
+    <>
       <Link href="/pokemon">back</Link>
       <CardContainer>
-        <BackgroundBall />
+        <BackgroundBall color={species.color.name} />
+        <CardImage
+          src={sprites.other["official-artwork"].front_default}
+          alt={name}
+          width={100}
+          height={100}
+        />
       </CardContainer>
-      <h2 style={{ color: species.color.name }}>
-        {name[0].toUpperCase() + name.slice(1)}
-      </h2>
-      <Image
-        src={sprites.other["official-artwork"].front_default}
-        alt={name}
-        width={100}
-        height={100}
-      />
-      {types.map((type) => (
-        <p key={type.type.name}>{type.type.name}</p>
-      ))}
-      {
+      <article>
+        <h2 style={{ color: species.color.name }}>
+          {name[0].toUpperCase() + name.slice(1)}
+        </h2>
+        <Image
+          src={sprites.other["official-artwork"].front_default}
+          alt={name}
+          width={100}
+          height={100}
+        />
+        {types.map((type) => (
+          <p key={type.type.name}>{type.type.name}</p>
+        ))}
+        {
+          <p>
+            {stats[0].base_stat} {stats[0].stat.name}
+          </p>
+        }
         <p>
-          {stats[0].base_stat} {stats[0].stat.name}
+          {stats[1].base_stat} {stats[1].stat.name}
         </p>
-      }
-      <p>
-        {stats[1].base_stat} {stats[1].stat.name}
-      </p>
-      <p>
-        {stats[2].base_stat} {stats[2].stat.name}
-      </p>
-      <p>
-        {stats[5].base_stat} {stats[5].stat.name}
-      </p>
-    </article>
+        <p>
+          {stats[2].base_stat} {stats[2].stat.name}
+        </p>
+        <p>
+          {stats[5].base_stat} {stats[5].stat.name}
+        </p>
+      </article>
+    </>
   );
 }
