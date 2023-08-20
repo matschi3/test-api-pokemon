2f62addfe6f7f67388dbeba80f47271394124283
Nils Fischer <nils@designatic.de>
2023-08-20 12:43:57 +0200

    reworked typeColors

--- styles.js
+++ styles.js
@@ -13,24 +13,24 @@ export default createGlobalStyle`
     --color-white: #eaeaea;
     --color-black: #121212;
     --color-main: rgb(255, 74, 17); 
-
-  
-  /* bug: "#26de81",
-  dragon: "#ffeaa7",
-  electric: "#fed330",
-  fairy: "#FF0069",
-  fighting: "#30336b",
-  fire: "#f0932b",
-  flying: "#81ecec",
-  grass: "#00b894",
-  ground: "#EFB549",
-  ghost: "#a55eea",
-  ice: "#74b9ff",
-  normal: "#95afc0",
-  poison: "#6c5ce7",
-  psychic: "#a29bfe",
-  rock: "#2d3436",
-  water: "#0190FF", */
+    
+// Pkmn type colors
+    --color-type-bug: #26de81;
+    --color-type-dragon: #ffeaa7;
+    --color-type-electric: #fed330;
+    --color-type-fairy: #FF0069;
+    --color-type-fighting: #30336b;
+    --color-type-fire: #f0932b;
+    --color-type-flying: #81ecec;
+    --color-type-grass: #00b894;
+    --color-type-ground: #EFB549;
+    --color-type-ghost: #a55eea;
+    --color-type-ice: #74b9ff;
+    --color-type-normal: #95afc0;
+    --color-type-poison: #6c5ce7;
+    --color-type-psychic: #a29bfe;
+    --color-type-rock: #2d3436;
+    --color-type-water: #0190FF;
 }
 
   body {
