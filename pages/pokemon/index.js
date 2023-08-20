a89771c17cec8b15e55d6ad90275eba7e68ecd86
Nils Fischer <nils@designatic.de>
2023-08-20 18:30:22 +0200

    revert to better and lightweight 'buttons' with icons

--- pages/pokemon/index.js
+++ pages/pokemon/index.js
@@ -1,10 +1,9 @@
 import useSWR from "swr";
 import { StyledButton } from "../../components/StyledButton/StyledButton.styled.js";
 import Image from "next/image.js";
-import { useState } from "react";
+import { CardContainer } from "../../components/PokemonCard/PokemonCard.styled.js";
 
 export default function PokemonPage() {
-  const [data, setData] = useState([]);
   const {
     data: fetchData,
     isLoading,
@@ -12,39 +11,28 @@ export default function PokemonPage() {
   } = useSWR("https://pokeapi.co/api/v2/pokemon");
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error</div>;
-
-  setData(
-    fetchData.results.map((result) => ({
-      id: result.url.match(/\/([0-9]*)\/$/)[1],
-      name: result.name,
-      image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-        result.url.match(/\/([0-9]*)\/$/)[1]
-      }.png`,
-    }))
-  );
-  console.log(data);
-  /* const { results } = data; */
+  const { results } = fetchData;
   return (
     <>
       <StyledButton href="/">back</StyledButton>
       <h1>Pokemons</h1>
-      <ul>
-        {data.map((result) => (
-          <li key={result.id}>
-            <StyledButton href={`/pokemon/${result.name}`}>
-              {
-                <Image
-                  href={result.image}
-                  alt={result.name}
-                  width={20}
-                  height={20}
-                />
-              }
-              {result.name[0].toUpperCase() + result.name.slice(1)}
-            </StyledButton>
-          </li>
+      <CardContainer>
+        {results.map((result) => (
+          <StyledButton key={result.name} href={`/pokemon/${result.name}`}>
+            <Image
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                result.url.match(/\/([0-9]*)\/$/)[1]
+              }.png`}
+              alt={result.name}
+              width={30}
+              height={30}
+              loading="lazy"
+              style={{ verticalAlign: "middle", marginRight: "0.5em" }}
+            />
+            {result.name[0].toUpperCase() + result.name.slice(1)}
+          </StyledButton>
         ))}
-      </ul>
+      </CardContainer>
     </>
   );
 }
