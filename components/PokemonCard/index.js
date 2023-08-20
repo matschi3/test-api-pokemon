767b9fd3bc36a6aca4d1be0683fe32465481d037
Nils Fischer <nils@designatic.de>
2023-08-20 13:49:01 +0200

    del unused/old 'card'

--- components/PokemonCard/index.js
+++ components/PokemonCard/index.js
@@ -71,34 +71,6 @@ export default function PokemonCard({ pokemon, species }) {
           </CardContainer>
         </Card>
       </CardContainer>
-      <article>
-        <h2 style={{ color: species.color.name }}>
-          {name[0].toUpperCase() + name.slice(1)}
-        </h2>
-        <Image
-          src={sprites.other["official-artwork"].front_default}
-          alt={name}
-          width={100}
-          height={100}
-        />
-        {types.map((type) => (
-          <p key={type.type.name}>{type.type.name}</p>
-        ))}
-        {
-          <p>
-            {stats[0].base_stat} {stats[0].stat.name}
-          </p>
-        }
-        <p>
-          {stats[1].base_stat} {stats[1].stat.name}
-        </p>
-        <p>
-          {stats[2].base_stat} {stats[2].stat.name}
-        </p>
-        <p>
-          {stats[5].base_stat} {stats[5].stat.name}
-        </p>
-      </article>
     </>
   );
 }
