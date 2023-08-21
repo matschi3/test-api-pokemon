abc2da2c4c4ad690a6b3ee3ac0c1c2482bac5c2b
Nils Fischer <nils@designatic.de>
2023-08-21 16:42:30 +0200

    add state for active button to display one single card for choosen set

--- components/TcgCards/index.js
+++ components/TcgCards/index.js
@@ -1,7 +1,10 @@
 import useSWR from "swr";
 import Image from "next/image";
+import { useState } from "react";
+import { StyledButton } from "../StyledButton/StyledButton.styled.js";
 
 export default function TcgCards({ pokemonName }) {
+  const [activeTcgCard, setActiveTcgCard] = useState("");
   const {
     data: tcg,
     isLoading: tcgIsLoading,
@@ -10,19 +13,29 @@ export default function TcgCards({ pokemonName }) {
   if (tcgIsLoading) return <div>Loading...</div>;
   if (tcgError) return <div>Error</div>;
   console.log(tcg);
+  console.log(activeTcgCard);
   return (
     <>
       {tcg.data.map((tcgCard) => (
+        <StyledButton
+          key={tcgCard.set.id}
+          onClick={() => setActiveTcgCard(`${tcgCard.images.large}`)}
+        >
+          {tcgCard.set.name}
+        </StyledButton>
+      ))}
+      {activeTcgCard === "" ? (
+        ""
+      ) : (
         <Image
-          key={tcgCard.id}
-          src={tcgCard.images.large}
+          src={activeTcgCard}
           alt={pokemonName}
           width={367}
           height={512}
           loading="lazy"
           style={{ margin: "0.5em", borderRadius: "0.5em" }}
         />
-      ))}
+      )}
     </>
   );
 }
