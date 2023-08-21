e1ac7cb74662daf2583c566b1bb3144c81381fdb
Nils Fischer <nils@designatic.de>
2023-08-21 15:48:53 +0200

    add next/Image for tcg render

--- components/TcgCards/index.js
+++ components/TcgCards/index.js
@@ -1,4 +1,5 @@
 import useSWR from "swr";
+import Image from "next/image";
 
 export default function TcgCards({ pokemonName }) {
   const {
@@ -8,5 +9,17 @@ export default function TcgCards({ pokemonName }) {
   } = useSWR(`https://api.pokemontcg.io/v2/cards/?q=name:${pokemonName}`);
   if (tcgIsLoading) return <div>Loading...</div>;
   if (tcgError) return <div>Error</div>;
-  return <p>TCG Cards for {pokemonName}</p>;
+  console.log(tcg);
+  return (
+    <>
+      <p>TCG Cards for {pokemonName}</p>
+      <Image
+        src={tcg.data[0].images.large}
+        alt={pokemonName}
+        width={367}
+        height={512}
+        loading="lazy"
+      />
+    </>
+  );
 }
