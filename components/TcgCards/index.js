bacf63bce4bbaf29451c3c61f0fae658b8dbb7e1
Nils Fischer <nils@designatic.de>
2023-08-21 15:33:24 +0200

    add useSWR to fetch data from pkmn tcg api

--- components/TcgCards/index.js
+++ components/TcgCards/index.js
@@ -1,3 +1,12 @@
+import useSWR from "swr";
+
 export default function TcgCards({ pokemonName }) {
+  const {
+    data: tcg,
+    isLoading: tcgIsLoading,
+    error: tcgError,
+  } = useSWR(`https://api.pokemontcg.io/v2/cards/?q=name:${pokemonName}`);
+  if (tcgIsLoading) return <div>Loading...</div>;
+  if (tcgError) return <div>Error</div>;
   return <p>TCG Cards for {pokemonName}</p>;
 }
