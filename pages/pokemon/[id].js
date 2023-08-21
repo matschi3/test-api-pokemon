190277f5b0382e4bd0f22076cba05a2bf0901f23
Nils Fischer <nils@designatic.de>
2023-08-21 14:44:14 +0200

    add button for tcg

--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -1,7 +1,7 @@
 import useSWR from "swr";
 import { useRouter } from "next/router";
 import PokemonCard from "../../components/PokemonCard/index.js";
-import { StyledButton } from "../../components/StyledButton/StyledButton.styled.js";
+import { StyledButton } from "../../components/StyledLinkButton/StyledLinkButton.styled.js";
 import { useState } from "react";
 
 export default function DetailPokemonPage() {
@@ -25,6 +25,11 @@ export default function DetailPokemonPage() {
     <>
       <StyledButton href="/pokemon">back</StyledButton>
       <PokemonCard pokemon={pokemon} species={species} />
+      {!tcgIsActive ? (
+        <button>Show TradingCards</button>
+      ) : (
+        <button>Hide TradingCards</button>
+      )}
     </>
   );
 }
