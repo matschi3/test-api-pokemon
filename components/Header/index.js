999c008eeef940b913df4fe83cce2b120a0682bc
Nils Fischer <nils@designatic.de>
2023-10-02 19:29:46 +0200

    rebuild header with navbar component

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,27 +1,64 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Col } from "react-bootstrap";
 import { StyledLinkButton } from "../StyledLinkButton/StyledLinkButton.styled";
 
 export default function Header() {
   return (
-    <Container>
-      <Row>
-        <Col xs={6} sm={6} md={{ order: 1, span: 4 }}>
-          <StyledLinkButton href="/">back</StyledLinkButton>
-        </Col>
-        <Col
-          xs={{ order: "first", span: 12 }}
-          sm={{ order: "first", span: 12 }}
-          md={{ order: 2, span: 4 }}
+    <Navbar sticky="top" expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src="/img/logo.svg"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{" "}
+          TCG-Dex
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className="justify-content-between"
         >
-          <span>TCG-Dex</span>
-        </Col>
-        <Col xs={6} sm={6} md={{ order: 3, span: 4 }}>
-          <span>Search/HideBtn</span>
-        </Col>
-      </Row>
-    </Container>
+          <div className="d-flex">
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Pokemon"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </div>
+          <div className="d-flex">
+            <Nav className="me-auto">
+              <Nav.Link href="/">back</Nav.Link>
+              <Nav.Link href="/pokemon">Pokemon+TCG</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
