71bf0c072cef273fc4a6c454704503b517055fc9
Nils Fischer <nils@designatic.de>
2023-10-02 22:44:56 +0200

    add state to display page/pokemonNmbrs

--- pages/pokemon/index.js
+++ pages/pokemon/index.js
@@ -4,8 +4,10 @@ import Image from "next/image.js";
 import { CardContainer } from "../../components/PokemonCard/PokemonCard.styled.js";
 import Header from "../../components/Header/index.js";
 import Pagination from "react-bootstrap/Pagination";
+import { useState } from "react";
 
 export default function PokemonPage() {
+  const [page, setPage] = useState("1-20");
   const {
     data: fetchData,
     isLoading,
@@ -38,21 +40,9 @@ export default function PokemonPage() {
       </CardContainer>
 
       <Pagination>
-        <Pagination.First />
         <Pagination.Prev />
-        <Pagination.Item>{1}</Pagination.Item>
-        <Pagination.Ellipsis />
-
-        <Pagination.Item>{10}</Pagination.Item>
-        <Pagination.Item>{11}</Pagination.Item>
-        <Pagination.Item active>{12}</Pagination.Item>
-        <Pagination.Item>{13}</Pagination.Item>
-        <Pagination.Item disabled>{14}</Pagination.Item>
-
-        <Pagination.Ellipsis />
-        <Pagination.Item>{20}</Pagination.Item>
+        <Pagination.Item active>{page}</Pagination.Item>
         <Pagination.Next />
-        <Pagination.Last />
       </Pagination>
     </>
   );
