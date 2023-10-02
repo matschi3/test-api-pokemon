7f4251ee1083ce94f2af43f7bec41a0160c0a47b
Nils Fischer <nils@designatic.de>
2023-10-02 22:35:07 +0200

    add basic pagination

--- pages/pokemon/index.js
+++ pages/pokemon/index.js
@@ -3,6 +3,7 @@ import { StyledLinkButton } from "../../components/StyledLinkButton/StyledLinkBu
 import Image from "next/image.js";
 import { CardContainer } from "../../components/PokemonCard/PokemonCard.styled.js";
 import Header from "../../components/Header/index.js";
+import Pagination from "react-bootstrap/Pagination";
 
 export default function PokemonPage() {
   const {
@@ -35,6 +36,24 @@ export default function PokemonPage() {
           </StyledLinkButton>
         ))}
       </CardContainer>
+
+      <Pagination>
+        <Pagination.First />
+        <Pagination.Prev />
+        <Pagination.Item>{1}</Pagination.Item>
+        <Pagination.Ellipsis />
+
+        <Pagination.Item>{10}</Pagination.Item>
+        <Pagination.Item>{11}</Pagination.Item>
+        <Pagination.Item active>{12}</Pagination.Item>
+        <Pagination.Item>{13}</Pagination.Item>
+        <Pagination.Item disabled>{14}</Pagination.Item>
+
+        <Pagination.Ellipsis />
+        <Pagination.Item>{20}</Pagination.Item>
+        <Pagination.Next />
+        <Pagination.Last />
+      </Pagination>
     </>
   );
 }
