2137027203c0e22056b8d0aa2aa5a82bc62ca3f1
Nils Fischer <nils@designatic.de>
2023-10-11 19:23:38 +0200

    del unwanted style

--- components/StyledLinkButton/StyledLinkButton.styled.js
+++ components/StyledLinkButton/StyledLinkButton.styled.js
@@ -8,17 +8,12 @@ export const StyledLinkButton = styled(StyledLink)`
   box-shadow: var(--color-black) 4px 4px 0 0;
   color: var(--color-black);
   cursor: pointer;
-  display: inline-block;
   font-weight: 600;
   font-size: 18px;
-  padding: 0 18px;
-  line-height: 50px;
-  text-align: center;
   text-decoration: none;
   user-select: none;
   -webkit-user-select: none;
   touch-action: manipulation;
-  margin: 0.15em;
 
   &:hover {
     background-color: var(--color-white);
@@ -28,10 +23,5 @@ export const StyledLinkButton = styled(StyledLink)`
     box-shadow: var(--color-gray) 2px 0 0;
     transform: translate(2px, 2px);
     border: 2px solid var(--color-grey);
-  }
-
-  @media (min-width: 768px) {
-    min-width: 120px;
-    padding: 0 25px;
   }
 `;
