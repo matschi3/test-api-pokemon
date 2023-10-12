30e313d71236e0bcb9cf96658416cd24abc339d0
Nils Fischer <nils@designatic.de>
2023-10-12 21:41:14 +0200

    add msg for no card to display

--- components/TcgCards/index.js
+++ components/TcgCards/index.js
@@ -4,29 +4,31 @@ import stringSimilarity from "string-similarity";
 export default function TcgCards({ tcg, activeSet }) {
   return (
     <>
-      {activeSet === ""
-        ? ""
-        : tcg.data.map((tcgCard) => {
-            const similarity = stringSimilarity.compareTwoStrings(
-              tcgCard.id,
-              activeSet
+      {activeSet === "" ? (
+        <p>Pick a TCG-Set to display a TradingCard</p>
+      ) : (
+        tcg.data.map((tcgCard) => {
+          const similarity = stringSimilarity.compareTwoStrings(
+            tcgCard.id,
+            activeSet
+          );
+          if (similarity > 0.6) {
+            // set a threshold for similarity
+            return (
+              <Image
+                key={tcgCard.id}
+                src={tcgCard.images.large}
+                alt={tcgCard.name}
+                width={367}
+                height={512}
+                loading="lazy"
+                style={{ margin: "0.5em", borderRadius: "0.5em" }}
+              />
             );
-            if (similarity > 0.6) {
-              // set a threshold for similarity
-              return (
-                <Image
-                  key={tcgCard.id}
-                  src={tcgCard.images.large}
-                  alt={tcgCard.name}
-                  width={367}
-                  height={512}
-                  loading="lazy"
-                  style={{ margin: "0.5em", borderRadius: "0.5em" }}
-                />
-              );
-            }
-            return null;
-          })}
+          }
+          return null;
+        })
+      )}
     </>
   );
 }
