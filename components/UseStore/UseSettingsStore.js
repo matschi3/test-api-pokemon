4030034d1cb9bc4b2450ed2e6e254ea90261096b
Nils Fischer <nils@designatic.de>
2023-10-12 14:21:24 +0200

    setup settings object in SettingsStore + setSettings to changes values according to setting

--- components/UseStore/UseSettingsStore.js
+++ components/UseStore/UseSettingsStore.js
@@ -1,5 +1,18 @@
 import { create } from "zustand";
 
-const UseSettingsStore = create();
+const UseSettingsStore = create(() => ({
+  settings: {
+    theme: "",
+    activeSet: "",
+  },
+  setSetting: (setting, newValue) => {
+    UseSettingsStore.setState((state) => ({
+      settings: {
+        ...state.settings,
+        [setting]: newValue,
+      },
+    }));
+  },
+}));
 
 export default UseSettingsStore;
