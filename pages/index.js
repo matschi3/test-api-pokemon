06f6a7136d77e14d6eaa0e18b35fb95c609e064a
Nils Fischer <nils@designatic.de>
2023-10-15 13:42:35 +0200

    setup information cad on index page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,36 @@
-import Link from "next/link";
+import Header from "@/components/Header";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 export default function HomePage() {
-  return <Link href="/pokemon">Pokemon</Link>;
+  return (
+    <>
+      <Header />
+      <Container>
+        <Row className="justify-content-center">
+          <Col md={9} lg={7} xl={6} xxl={5}>
+            <Card className="text-center">
+              <Card.Header>Hello there!</Card.Header>
+              <Card.Body>
+                <Card.Title>Check out Pokemon Trading Cards here</Card.Title>
+                <Card.Text>
+                  By scrolling down a list of Pkmn or by searching for a
+                  specific one.
+                </Card.Text>
+                <Button variant="primary" href="/pokemon">
+                  Pick a Pokemon
+                </Button>
+              </Card.Body>
+              <Card.Footer className="text-muted">
+                Got your Pkmn? Pick a Set!
+              </Card.Footer>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
