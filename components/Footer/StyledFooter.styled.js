9a45d46ba89665b4e41617854ad6148497b6a6e6
Nils Fischer <nils@designatic.de>
2023-10-20 15:07:52 +0200

    init footer

---
+++ components/Footer/StyledFooter.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledFooter = styled.section`
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: #333;
+  color: white;
+`;
